bb13cb561a01c7c9142a5aae1ff7dc10e34e18ec
Naveen Kumar Kaushik <Naveen.Kaushik@office365.3pillarglobal.com>
2018-05-11 14:07:47 +0530

    Minor comments updated

--- springbootservice1/src/main/java/com/tpg/microservices/service1/api/Api.java
+++ springbootservice1/src/main/java/com/tpg/microservices/service1/api/Api.java
@@ -1,6 +1,5 @@
 package com.tpg.microservices.service1.api;
 
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class Api {
 
 	/**
-	 * This method is called by root path / of the service, like localhost:2221/service1
+	 * This method is called to check the service availability and 
+	 * can also be configured with Application Load Balancer to check
+	 * the service health
+	 *
+	 * Can be called like localhost:2221/service1
 	 * 
 	 * @return return a string value
 	 */
--- springbootservice2/src/main/java/com/tpg/microservices/service2/api/Api.java
+++ springbootservice2/src/main/java/com/tpg/microservices/service2/api/Api.java
@@ -7,7 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class Api {
 
 	/**
-	 * This method is called by root path / of the service, like localhost:2222/service2
+	 * This method is called to check the service availability and 
+	 * can also be configured with Application Load Balancer to check
+	 * the service health
+	 *
+	 * Can be called like localhost:2222/service2
 	 * 
 	 * @return return a string value
 	 */
